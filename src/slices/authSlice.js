dd1cc5bb1a41c8d5c440cdeb07d23ece9ed79abe
Saniya2701 <185913688+Saniya2701@users.noreply.github.com>
2026-03-05 12:26:31 +0530

    Fix authSlice missing error

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
+  signupData: null,
   token: localStorage.getItem("token")
     ? JSON.parse(localStorage.getItem("token"))
     : null,
@@ -11,6 +12,9 @@ const authSlice = createSlice({
   name: "auth",
   initialState: initialState,
   reducers: {
+    setSignupData: (state, value) => {
+      state.signupData = value.payload
+    },
     setToken: (state, value) => {
       state.token = value.payload
     },
@@ -20,5 +24,5 @@ const authSlice = createSlice({
   },
 })
 
-export const { setToken, setLoading } = authSlice.actions
+export const { setSignupData, setToken, setLoading } = authSlice.actions
 export default authSlice.reducer
